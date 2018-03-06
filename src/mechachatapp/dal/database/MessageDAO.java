980416d591ae146dfb27d46708bebf5c8b712175
Stegger <stegger.peter@gmail.com>
2018-03-06 11:04:45 +0100

    Began DB DAOs

---
+++ src/mechachatapp/dal/database/MessageDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mechachatapp.dal.database;
+
+/**
+ *
+ * @author pgn
+ */
+public class MessageDAO
+{
+    
+}
---
+++ src/mechachatapp/dal/facade/MCDatabaseDalFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mechachatapp.dal.facade;
+
+import java.util.List;
+import mechachatapp.be.Message;
+
+/**
+ *
+ * @author pgn
+ */
+public class MCDatabaseDalFacade implements IMechaChatDalFacade
+{
+
+    @Override
+    public Message createMessage(String msg)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Message> readAllMessages()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
