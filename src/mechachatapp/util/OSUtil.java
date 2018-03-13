826b10ace6c4a1e2e674f7276e35334ef2534c59
mjl <mjl@easv.dk>
2018-03-13 09:54:21 +0100

    OSUtil needed for keycomb stuff

---
+++ src/mechachatapp/util/OSUtil.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mechachatapp.util;
+
+/**
+ *
+ * @author mjl
+ */
+public final class OSUtil 
+{
+   private static String OS = null;
+   public static String getOsName()
+   {
+      if(OS == null) { OS = System.getProperty("os.name"); }
+      return OS;
+   }
+   public static boolean isWindows()
+   {
+      return getOsName().startsWith("Windows");
+   }
+}
